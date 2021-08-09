74192ff79e455f1a1a264c33cf3055971d000afa
Michael Crates <michaelcrates89@hotmail.co.uk>
2021-08-09 19:59:45 +0100

    homepage layout mapped

--- src/pages/homepage/index.js
+++ src/pages/homepage/index.js
@@ -1,5 +1,32 @@
+import "./index.css";
+
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <div className="home-container">
+      <div className="home-content">
+        <h1>
+          Business
+          <br /> With Pride!
+        </h1>
+      </div>
+      <div className="home-content">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+          facilisis porta condimentum. Vestibulum vitae pretium nibh. Sed et
+          sagittis turpis. Vivamus magna lectus, hendrerit in enim et, aliquam
+          tempus ligula. In mi nunc, pulvinar sit amet feugiat nec, dignissim
+          sed lacus. Class aptent taciti sociosqu ad litora torquent per conubia
+          nostra, per inceptos himenaeos. Donec consectetur blandit magna, et
+          scelerisque est dignissim sit amet. Aenean semper non ligula eu
+          dapibus. Nullam dui sapien, pharetra vitae varius pretium, porta at
+          justo. Donec at aliquet nunc, quis tristique nisl. Suspendisse
+          bibendum sollicitudin pulvinar. Nulla et odio neque. Vivamus congue eu
+          lectus vel ultricies. Pellentesque orci leo, venenatis sed elit in,
+          varius posuere magna. About us/About site!!!!
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Homepage;
