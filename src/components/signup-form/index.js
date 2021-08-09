452a2f2decfa906bb9c5d3ed2a4251613d8e1228
Hakkelo89 <78032311+Hakkelo89@users.noreply.github.com>
2021-08-09 20:42:23 +0100

    add logic to signup form (wip)

--- src/components/signup-form/index.js
+++ src/components/signup-form/index.js
@@ -2,12 +2,37 @@ import React from "react";
 import { useForm } from "react-hook-form";
 
 import Button from "../button";
+import { SIGNUP } from "../../mutations";
 
 import "./index.css";
+import { useMutation } from "@apollo/client";
 
 const SignUpForm = (props) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const [signup, { data, loading, error }] = useMutation(SIGNUP, {
+    onCompleted: () => {
+      console.log(data);
+    },
+    onError: () => {
+      console.log("error");
+    },
+  });
+
+  const onSubmit = async (formData) => {
+    await signup({
+      variables: {
+        signupInput: formData,
+      },
+    });
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="signup-form">
         <p>
           Thanks for choosing to sign up! Please enter the details below and we
