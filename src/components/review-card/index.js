42840d1e21d35bfa359eca4cc7273538600b9b8c
Michael Crates <michaelcrates89@hotmail.co.uk>
2021-08-10 21:05:24 +0100

    added calendly and updated styling

--- src/components/review-card/index.js
+++ src/components/review-card/index.js
@@ -9,13 +9,13 @@ const ReviewCard = (props) => {
   return (
     <div className="review-card-container">
       <div className="review-card-rating">
-        <h1>Rating:4.5/5</h1>
+        <h3>Rating:4.5/5</h3>
         <div>
           <ReactStars
             count={5}
             edit={false}
             value={4.5}
-            size={24}
+            size={25}
             activeColor="#f2b5d4"
             isHalf={true}
           />
--- src/pages/user/index.js
+++ src/pages/user/index.js
@@ -1,28 +1,31 @@
 import Avatar from "../../components/avatar";
+import Calendly from "../../components/calendly";
 import SimpleModal from "../../components/modal";
 import NewsFeedCard from "../../components/newsfeed-card";
 import ReviewCard from "../../components/review-card";
+
+import "./index.css";
 
 const UserProfile = () => {
   return (
     <>
       <div className="profile-container">
-        <div>
-          <h3>Username/business name</h3>
-          <small>hello lee</small>
-          <div>Business info</div>
+        <div className="profile-left-all">
+          <h1 className="profile-left">Username/business name</h1>
+          <ReviewCard />
+          <div className="profile-left">about me!</div>
+          <div className="profile-left">Business info</div>
+          <SimpleModal name="Followers" />
         </div>
         <div>
           <Avatar URL="https://pbs.twimg.com/profile_images/1290710495465541633/BhrDfujl_400x400.jpg" />
           <SimpleModal name="Contact Me" />
         </div>
         <div>
-          <div>calender</div>
-          <ReviewCard />
-          <SimpleModal name="Followers" />
+          <Calendly />
         </div>
       </div>
-      <div>
+      <div className="profile-card">
         <NewsFeedCard
           title="Hello"
           body="Welcome to my first post!"
