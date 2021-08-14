add921652f245a2c98008945f95ce2e335977af4
Michael Crates <michaelcrates89@hotmail.co.uk>
2021-08-14 13:46:54 +0100

    private navbar logout function

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -10,12 +10,13 @@ const Navbar = (props) => {
   const { state, dispatch } = useUserContext();
   const handleLogout = () => {
     localStorage.removeItem("user");
+    localStorage.removeItem("id_token");
     dispatch({ type: "LOGOUT" });
+    window.location.replace("/");
   };
   return (
     <div>
-      {!state.user && <div>Navbar</div>}
-      {state.user && (
+      {!state.user && (
         <div className="navbar">
           <div className="title">{props.name}</div>
           <div>
@@ -25,6 +26,19 @@ const Navbar = (props) => {
             <Button name="Home" />
             <Button name="Log In" />
             <Button name="Sign Up" />
+          </div>
+        </div>
+      )}
+      {state.user && (
+        <div className="navbar">
+          <div className="title">{props.name}</div>
+          <div>
+            <SearchBar />
+          </div>
+          <div>
+            <Button name="Dashboard" />
+            <Button name="Profile" />
+            <Button name="Log Out" onClick={handleLogout} />
           </div>
         </div>
       )}
