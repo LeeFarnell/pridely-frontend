ca38e860ab7afa0eb564b74b4f71018a58ec5e02
Lee Farnell <leefarnell@mac.com>
2021-08-25 20:12:17 +0100

    amended text on home page

--- src/pages/homepage/index.js
+++ src/pages/homepage/index.js
@@ -10,20 +10,17 @@ const Homepage = () => {
         </h1>
       </div>
       <div className="home-content">
+        <h2>Welcome to Pridely.</h2>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-          facilisis porta condimentum. Vestibulum vitae pretium nibh. Sed et
-          sagittis turpis. Vivamus magna lectus, hendrerit in enim et, aliquam
-          tempus ligula. In mi nunc, pulvinar sit amet feugiat nec, dignissim
-          sed lacus. Class aptent taciti sociosqu ad litora torquent per conubia
-          nostra, per inceptos himenaeos. Donec consectetur blandit magna, et
-          scelerisque est dignissim sit amet. Aenean semper non ligula eu
-          dapibus. Nullam dui sapien, pharetra vitae varius pretium, porta at
-          justo. Donec at aliquet nunc, quis tristique nisl. Suspendisse
-          bibendum sollicitudin pulvinar. Nulla et odio neque. Vivamus congue eu
-          lectus vel ultricies. Pellentesque orci leo, venenatis sed elit in,
-          varius posuere magna. About us/About site!!!!
+          Pridely is your new app, to help you find local businesses, which
+          support and champion the LGBTQIA+ community.
         </p>
+        <p>
+          For too long, businesses have been able to discriminate against the
+          LGBTQIA+ community, leaving people feeling unwelcome & unsupported. We
+          aim to change that.
+        </p>
+        <p>At Pridely, everyone is welcome.</p>
       </div>
     </div>
   );
