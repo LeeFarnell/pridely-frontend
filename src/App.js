5249db3ab068b6681c3f9148d8dd9c0b0d936528
Lee Farnell <leefarnell@mac.com>
2021-08-26 23:10:51 +0100

    Updated uri

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ import Footer from "./components/footer";
 import "./App.css";
 
 const httpLink = createHttpLink({
-  uri: process.env.GRAPHQL_URL || "http://localhost:4000/graphql",
+  // uri: process.env.GRAPHQL_URL || "http://localhost:4000/graphql",
+  uri: "/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
