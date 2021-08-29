e68b248cdda529e1e7e2c03e3daec31faccab545
Tudor Tocan <tudor.tocan@gmail.com>
2021-08-29 19:34:55 +0100

    add styled loader

--- src/components/loading/index.js
+++ src/components/loading/index.js
@@ -1,22 +1,50 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import CircularProgress from "@material-ui/core/CircularProgress";
+// import React from "react";
+// import { makeStyles } from "@material-ui/core/styles";
+// import CircularProgress from "@material-ui/core/CircularProgress";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-    "& > * + *": {
-      marginLeft: theme.spacing(2),
-    },
-  },
-}));
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     display: "flex",
+//     "& > * + *": {
+//       marginLeft: theme.spacing(2),
+//     },
+//   },
+// }));
 
-export default function CircularIndeterminate() {
-  const classes = useStyles();
+// export default function CircularIndeterminate() {
+//   const classes = useStyles();
 
+//   return (
+//     <div className={classes.root}>
+//       <CircularProgress color="primary" />
+//     </div>
+//   );
+// }
+
+import "./loading-spinner.css";
+
+const LoadingSpinner = () => {
   return (
-    <div className={classes.root}>
-      <CircularProgress color="primary" />
+    <div className="loader">
+      <div className="loader-inner">
+        <div className="loader-line-wrap">
+          <div className="loader-line"></div>
+        </div>
+        <div className="loader-line-wrap">
+          <div className="loader-line"></div>
+        </div>
+        <div className="loader-line-wrap">
+          <div className="loader-line"></div>
+        </div>
+        <div className="loader-line-wrap">
+          <div className="loader-line"></div>
+        </div>
+        <div className="loader-line-wrap">
+          <div className="loader-line"></div>
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+export default LoadingSpinner;
