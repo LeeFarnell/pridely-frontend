4d1b9cbfcebc727e39b276bc29ee0afd7a1f7e5d
Lee Farnell <leefarnell@mac.com>
2021-08-31 20:35:40 +0100

    amended app.js to change URI

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ import UserProvider from "./contexts/UserProvider";
 import "./App.css";
 
 const httpLink = createHttpLink({
-  uri: process.env.GRAPHQL_URL || "http://localhost:4000/graphql",
-  // uri: "https://calm-dusk-10998.herokuapp.com/graphql",
+  // uri: process.env.GRAPHQL_URL || "http://localhost:4000/graphql",
+  uri: "https://calm-dusk-10998.herokuapp.com/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
